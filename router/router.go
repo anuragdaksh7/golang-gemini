feb18239ab65b5a47466c1ff87436b09bee7995a
anuragdaksh7 <anuragdaksh77777@gmail.com>
2024-11-27 23:26:58 +0530

    added / get

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"gemini-go/internal/gemini"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var r *gin.Engine
@@ -11,6 +12,9 @@ func InitRouter(geminiHandler *gemini.Handler) {
 	r = gin.Default()
 
 	r.POST("/query", geminiHandler.GetResponse)
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello from gemini-go-serv"})
+	})
 }
 
 func Start(addr string) error {
